9a2cf58c122c37af7ebf7c7cf1cbc029861c24d0
Hannes Huanca <HANNESHUANCA987gmail.com>
2022-04-27 17:00:48 -0400

    CON VERIFICACION DE NOMBRE Y APELLIDO

--- app/src/main/java/com/example/registrounivalle_a1/MainActivity.java
+++ app/src/main/java/com/example/registrounivalle_a1/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     private Button btnRegistrar;
     private Switch swEstudiante;
 
+    private String nombre,apellido,mail;
+    private int celular;
+    private int nota;
 
 
     @Override
@@ -21,7 +25,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         inicializarVistas();
+
+        btnRegistrar.setOnClickListener((view)->{verificarLlenado();});
     }
+
+    private void verificarLlenado() {
+
+        nombre=etNombre.getText().toString().trim();
+        apellido=etApellido.getText().toString().trim();
+        if(apellido.matches("")||apellido==null||nombre.matches("")||nombre==null){
+            Toast.makeText(this,"Debe ingresar datos!!",Toast.LENGTH_LONG).show();
+        }
+        else{
+
+                nombre=etNombre.getText().toString();
+                apellido=etApellido.getText().toString();
+
+
+            //mail=etEmail.getText().toString();
+            //celular=Integer.parseInt(etTelefono.getText().toString());
+            //nota=Integer.parseInt(etNota.getText().toString());
+            mostrarValores();
+        }
+
+    }
+
+    private void mostrarValores() {
+        Toast.makeText(this,
+                "El nombre es: "+nombre+
+                "\n, el apellido es: "+apellido+
+                "\n, el correo es: "+mail+
+                "\n, el celular es: "+celular+
+                "\n y la nota es: "+nota,Toast.LENGTH_SHORT).show();
+    }
+
+    private void recibirInformacion() {
+        nombre=etNombre.getText().toString();
+        apellido=etApellido.getText().toString();
+        mail=etEmail.getText().toString();
+        celular=Integer.parseInt(etTelefono.getText().toString());
+        nota=Integer.parseInt(etNota.getText().toString());
+
+    }
+
 
     private void inicializarVistas() {
         etNombre=findViewById(R.id.etNombre);
@@ -31,4 +77,7 @@ public class MainActivity extends AppCompatActivity {
         btnRegistrar=findViewById(R.id.btnRegistrar);
         swEstudiante=findViewById(R.id.swEstudiante);
     }
+
+
+
 }
